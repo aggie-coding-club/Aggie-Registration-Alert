574940992f4dde8e8f358aeab97ccfc47d2bca6c
zhuodannychen <zhuodannychen@gmail.com>
2020-09-24 12:37:40 -0500

    setup mongodb

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+require('dotenv').config();
+
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(bodyParser.json());
+// DB Config
+// Connect to MongoDB
+
+console.log("hello", process.env.MONGODB_URI)
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true, }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
