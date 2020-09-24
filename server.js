003aa311befa8d550f41c3d69a9823472bff16df
zhuodannychen <zhuodannychen@gmail.com>
2020-09-24 16:33:06 -0500

    edit

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(bodyParser.json());
 // DB Config
 // Connect to MongoDB
 
-console.log("hello", process.env.MONGODB_URI)
-
 mongoose
   .connect(
     process.env.MONGODB_URI,
@@ -29,3 +27,4 @@ mongoose
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
