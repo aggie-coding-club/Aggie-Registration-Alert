d4bbd37b7faaf9c135c94d6057da5a8922946eb5
zhuodannychen <zhuodannychen@gmail.com>
2020-10-09 23:22:57 -0500

    added login boilerplate

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const app = express();
+const passport = require("passport");
 require('dotenv').config();
 
+const users = require("./routes/api/users");
+
+const app = express();
 
 // Bodyparser middleware
 app.use(
@@ -11,19 +14,30 @@ app.use(
     extended: false
   })
 );
-
 app.use(bodyParser.json());
+
 // DB Config
 // Connect to MongoDB
 
+db = process.env.MONGODB_URI
+
 mongoose
   .connect(
-    process.env.MONGODB_URI,
+    db,
     { useNewUrlParser: true,
       useUnifiedTopology: true, }
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
